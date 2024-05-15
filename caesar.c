06a170dce38a25c41ff381531cb75af29f862ed6
abkavi9924 <165765669+abkavi9924@users.noreply.github.com>
2024-05-16 02:08:35 +0530

    Update caesar.c

--- caesar.c
+++ caesar.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-// Function to encrypt the message using Caesar
+
 void caesarEnc(char message[], int shift) {
     int i;
     char chr;
     for(i = 0; message[i] != '\0'; ++i) {
         chr = message[i];
-        // Encrypt uppercase letters
         if(chr >= 'A' && chr <= 'Z')
             message[i] = (chr + shift - 'A') % 26 + 'A';
-        // Encrypt lowercase letters
         else if(chr >= 'a' && chr <= 'z')
             message[i] = (chr + shift - 'a') % 26 + 'a';
     }
@@ -21,7 +19,6 @@ int main() {
     fgets(message, sizeof(message), stdin);
     printf("Enter the shift value: ");
     scanf("%d", &shift);
-    // Ensure shift is within the range of 0 to 25
     shift = shift % 26;
     caesarEnc(message, shift);
     printf("Encrypted message: %s\n", message);
